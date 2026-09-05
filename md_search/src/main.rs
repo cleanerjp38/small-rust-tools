0a7487c82b0dd12f22f1b42039abeb1e2f0226ed
cleanerjp38 <cleanerjp38@gmail.com>
2026-09-05 19:24:28 +0900

    変更002

--- md_search/src/main.rs
+++ md_search/src/main.rs
@@ -3,6 +3,11 @@ use std::fs;
 fn main() {
     let content = fs::read_to_string("Cargo.toml").unwrap();
 
-    println!("---ファイルの中身---");
-    println!("{}", content);
+    let keyword = "workspace";
+
+    if content.contains(keyword){
+        println!("「{}」が見つかりました！", keyword);
+    } else {
+        println!("「{}」は見つかりませんでした", keyword);
+    }
 }
