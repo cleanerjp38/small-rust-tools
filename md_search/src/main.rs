11d60d2e97f3e51b7baafb4309cca8903b66b122
cleanerjp38 <cleanerjp38@gmail.com>
2026-09-17 15:49:55 +0900

    変更011

--- md_search/src/main.rs
+++ md_search/src/main.rs
@@ -4,14 +4,6 @@ use std::env;
 fn main() {
     let content = fs::read_to_string("Cargo.toml").unwrap();
 
-    let keyword = "workspace";
-
-    if content.contains(keyword){
-        println!("「{}」が見つかりました！", keyword);
-    } else {
-        println!("「{}」は見つかりませんでした", keyword);
-    }
-
     //env::args()はどういう操作なんだろう？ターミナル系の操作なのかな
     let args: Vec<String> = env::args().collect();//cargo run -- testと入力すると、argsに要素が詰まった。
     //rust_analyzerでrunしたら、ターミナル操作ではなかったので、testは出力されなかった。
@@ -21,18 +13,26 @@ fn main() {
     println!("検索先フォルダ:{}", target_dir);
     let entries = fs::read_dir(target_dir).unwrap();
     println!("{:?}", entries);//これだとReadDir(".")が出力された
+    
+    let keyword = &args[2];//コマンドライン引数で、Markdown内の検索ワードを変数に入れる
+
+    if content.contains(keyword){
+        println!("「{}」が見つかりました！", keyword);
+    } else {
+        println!("「{}」は見つかりませんでした", keyword);
+    }
 
     for entry in entries {
-        println!("{:?}", entry);//Ok(DirEntry(".\\.git"))
+        //println!("{:?}", entry);//Ok(DirEntry(".\\.git"))
         let entry = entry.unwrap();
-        println!("{:?}", entry);//DirEntry(".\\.git")
+        //println!("{:?}", entry);//DirEntry(".\\.git")
 
         let path = entry.path();
-        println!("{:?}", path);//".\\.git"
-        println!("{:?}", path.extension());//None
+        //println!("{:?}", path);//".\\.git"
+        //println!("{:?}", path.extension());//None
         if path.extension() == Some(std::ffi::OsStr::new("md")) {
-            println!("Markdown発見: {:?}", path);//現在、md_searchフォルダにmdファイルはないので、なにも表示されない
-            println!("ファイル名: {:?}", path.file_name());//もしmdファイルがあれば、名前を表示する
+            //println!("Markdown発見: {:?}", path);//現在、md_searchフォルダにmdファイルはないので、なにも表示されない
+            //println!("ファイル名: {:?}", path.file_name());//もしmdファイルがあれば、名前を表示する
         }
     }
 }
